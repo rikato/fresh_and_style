890bb410a3fdc74af2b9969b1fc06b3463dcfeac
rikato <rikato@live.nl>
2017-11-28 01:24:31 +0100

    added adjai login

--- login.php
+++ login.php
@@ -38,30 +38,10 @@ if (isset($_POST['Login'])){
     }
 }
 
+
 ?>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/styles/custom.css">
-
-<div class="container">
-    <h1>Login</h1>
-    <form method="post" action="login.php">
-        <div class="form-group">
-            <input class="form-control" type="text" name="Username"><br>
-
-        </div>
-        <div class="form-group">
-            <input class="form-control" type="password" name="Password"><br>
-        </div>
-        <button type="submit" name="Loign" class="btn btn-primary">Login</button>
-    </form>
-</div>
-
-<!--Scripts-->
-<!--Bootstrap-->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-<!--Custom-->
-<script src="assets/javascript/custom.js"></script>
+<form method="post" action="login.php">
+    <input type="text" name="Username"><br>
+    <input type="password" name="Password"><br>
+    <input type="submit" name="Login" value="Login">
+</form>
